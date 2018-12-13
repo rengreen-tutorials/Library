5a7603efb03b65994ce9b8aba8fd9532bf011a02
rengreen <r.m.gardzinska@gmail.com>
2018-12-13 20:02:12 +0100

    adding method that returns data about the book

--- books/src/main/kotlin/pl/rengreen/library/books/BookService.kt
+++ books/src/main/kotlin/pl/rengreen/library/books/BookService.kt
@@ -4,5 +4,15 @@ import org.springframework.stereotype.Service
 
 @Service
 class BookService(private val bookRepository: BookRepository) {
-    fun getAvailableBooks() = bookRepository.findAll().filter { it.available }
+
+    fun getAvailableBooks() = bookRepository.findAll().filter {
+        it.available }
+
+    fun getBookInfo(bookId: Long): BookDTO =
+            with(bookRepository.findById(bookId)) {
+                when {
+                    this.isPresent -> this.get().toDTO()
+                    else -> throw NoSuchElementException("Book with id $bookId does not exist")
+                }
+            }
 }
