0a63d63f4414406c70868c6b54d56416929eac65
rengreen <r.m.gardzinska@gmail.com>
2018-12-14 01:36:34 +0100

    adding methods that change the book's availability status to opposed

--- books/src/main/kotlin/pl/rengreen/library/books/BookService.kt
+++ books/src/main/kotlin/pl/rengreen/library/books/BookService.kt
@@ -26,4 +26,32 @@ class BookService(private val bookRepository: BookRepository) {
 
     fun addBook(title: String, author: String, publicationYear: Int) =
             bookRepository.save(Book(0, title, author, publicationYear, true))
+
+    private fun toggleBookStatus(book: Book) =
+            bookRepository.save(book.copy(available = !book.available))
+
+    fun markBookAsAvailable(bookId: Long) =
+            with(bookRepository.findById(bookId)) {
+                if (this.isPresent) {
+                    when {
+                        this.get().available -> throw IllegalStateException("Book is already available")
+                        else -> toggleBookStatus(this.get())
+                    }
+                } else {
+                    throw NoSuchElementException("Book with id $bookId does not exist")
+                }
+            }
+    fun markBookAsUnavailable(bookId: Long) =
+            with(bookRepository.findById(bookId)) {
+                if (this.isPresent) {
+                    when {
+                        !this.get().available -> throw IllegalStateException("Book is already unavailable")
+                        else -> toggleBookStatus(this.get())
+                    }
+                } else {
+                    throw NoSuchElementException("Book with id $bookId does not exist")
+                }
+            }
+
+
 }
