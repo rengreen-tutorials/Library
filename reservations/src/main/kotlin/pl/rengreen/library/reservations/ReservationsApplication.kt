514c0dfb3f27b484da14ff10fc52e89b8e63d7f2
rengreen <r.m.gardzinska@gmail.com>
2018-12-15 10:16:02 +0100

    starting reservations microservice

---
+++ reservations/src/main/kotlin/pl/rengreen/library/reservations/ReservationsApplication.kt
@@ -0,0 +1,12 @@
+package pl.rengreen.library.reservations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReservationsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReservationsApplication>(*args)
+}
+
