735d5aa31b08fb9b29f342a8ebcaf8921f0884c1
rengreen <r.m.gardzinska@gmail.com>
2018-12-19 17:58:32 +0100

    adding 3 methods to interface ReservationRepository

--- reservations/src/main/kotlin/pl/rengreen/library/reservations/Repositories.kt
+++ reservations/src/main/kotlin/pl/rengreen/library/reservations/Repositories.kt
@@ -3,4 +3,9 @@ package pl.rengreen.library.reservations
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository: JpaRepository<User, Long>
-interface ReservationRepository: JpaRepository<Reservation, Long>
+
+interface ReservationRepository: JpaRepository<Reservation, Long> {
+    override fun findAll(): List<Reservation>
+    fun findAllByUser_Id(id: Long): List<Reservation>
+    fun findAllByBookId(bookId: Long): List<Reservation>
+}
